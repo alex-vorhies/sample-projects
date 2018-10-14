52a585b6529e42495b1915e8e0b7ca649a78ec4e
Alex Vorhies <44144310+alex-vorhies@users.noreply.github.com>
2018-10-14 16:15:13 -0500

    Remove proposed points

--- Tweet Search/tweet.cpp
+++ Tweet Search/tweet.cpp
@@ -1,7 +1,6 @@
 //Name: Alex Vorhies
 //Date: 4/11/16
 //Description: Searches a file of tweets and adds them to a list for use later (requires tweet.h, main.cpp, tweetlist.h, and tweetlist.cpp to work)
-//Proposed points: I feel I should receive 20 points
 
 #include "tweet.h" //include the Tweet header file
 #include <fstream> //required for the program to use files
